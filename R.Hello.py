dd3b316c355706ca1fbc9a61e49347b1212fc556
Adam Noonan <adamcnoonan@gmail.com>
2018-11-25 19:19:46 -0500

    Add files via upload

--- R.Hello.py
+++ R.Hello.py
@@ -2,3 +2,14 @@ _author_ = 'dev'
 greeting = "Hello"
 name = raw_input("Please enter your name ")
 print(greeting + ' ' + name)
+#Using user input as a string.
+
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+#Using string split, assigned to a variable
+
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+#Using tab in strings
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\s resting"')
