411f78fb19a862f98fecd48f56513cee3dee62b3
Adam Noonan <adamcnoonan@gmail.com>
2018-11-25 19:26:16 -0500

    Add files via upload

--- R.Hello.py
+++ R.Hello.py
@@ -12,4 +12,11 @@ tabbedString = "1\t2\t3\t4\t5\t"
 print(tabbedString)
 #Using tab in strings
 
-print('The pet shop owner said "No, no, \'e\'s uh,...he\s resting"')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+
+
